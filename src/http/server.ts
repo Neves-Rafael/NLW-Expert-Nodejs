0ec857dc2c3e7c5498f4f6daea9bb40752cd24de
rafael neves <nevesrafael.dev@gmail.com>
2024-02-06 11:26:17 -0300

    :sparkles: Feat http route get-poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,26 +1,11 @@
 import fastify from "fastify";
-import { z } from "zod";
-import { PrismaClient } from "@prisma/client";
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
 
 const app = fastify();
 
-const prisma = new PrismaClient();
-
-app.post("/polls", async (request, reply) => {
-  const createPollBody = z.object({
-    title: z.string(),
-  });
-
-  const { title } = createPollBody.parse(request.body);
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    },
-  });
-
-  return reply.status(201).send({ pollId: poll.id });
-});
+app.register(createPoll);
+app.register(getPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Http server running!");

3c2cf4bca69d4dc95e06dfd06184013216747ab6
rafael neves <nevesrafael.dev@gmail.com>
2024-02-06 11:34:58 -0300

    :sparkles: Feat vote-on-poll in http routes

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,20 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../../lib/prisma";
+import { z } from "zod";
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post("/polls/:pollId/votes", async (request, reply) => {
+    const voteOnPollBody = z.object({
+      pollOptionId: z.string().uuid(),
+    });
+
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid(),
+    });
+
+    const { pollId } = voteOnPollParams.parse(request.params);
+    const { pollOptionId } = voteOnPollBody.parse(request.body);
+
+    return reply.status(201).send();
+  });
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,13 @@
 import fastify from "fastify";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
 
 const app = fastify();
 
 app.register(createPoll);
 app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Http server running!");
